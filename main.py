e705fef46fba76ec9ed15f46847bc38d30eaa713
DESKTOP-48HRHH4\202-19 <tkdnjsrl2@gmail.com>
2023-06-21 12:29:18 +0900

    완성

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import random
 lotto_num = range(1,46)
 
 def buttonClick():
-    print(random.sample(lotto_num,6))
-
+    label =tkinter.Label(window, text=str(random.sample(lotto_num,6)))
+    label.pack()
 window = tkinter.Tk()
 window.title("lotto")
 window.geometry("400x200")
